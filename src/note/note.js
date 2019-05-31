4db70ce51acdc943b18a4b535b365ba38e2921d3
Dustin Haefele <Dustin.haefele@gmail.com>
2019-05-31 14:09:10 -0400

    erased a comment

--- src/note/note.js
+++ src/note/note.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import UserContext from '../UserContext';
 import { format } from 'date-fns';
-// import { format } from 'upath';
 
 export default class Note extends React.Component {
   static contextType = UserContext;
@@ -10,8 +9,6 @@ export default class Note extends React.Component {
   const noteId = this.props.match.params.noteId;
   const filteredNotes = this.context.notes.filter(note => note.id === noteId);
   const note = filteredNotes.map(note => {
-    // const newDate = new Date(Date.parse(note.modified))
-    // debugger;
     return (
     <div className='note' key={note.id}>
       <h3>{note.name}</h3>
